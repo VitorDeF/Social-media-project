cb0d8471ae8e2a2fcb830e716c3c349ac52fbda0
VitorDeF <vitorfigp@gmail.com>
2024-02-23 18:21:25 -0300

    Routes creation and authorization loggin setup

--- server/index.js
+++ server/index.js
@@ -39,6 +39,9 @@ const upload = multer({storage});
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 
+/* ROUTES */
+app.use("/auth", authRoutes);
+
 console.log(process.env.MONGO_URL)
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
